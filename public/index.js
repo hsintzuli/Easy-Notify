fae3948375546f9b9f8efc4abd8053fb0405c597
hsintzuli <hsintzuli719@gmail.com>
2022-04-16 16:43:22 +0800

    feat: adjust subscription mechanism in front-end to correctly unsubscribe

--- public/index.js
+++ public/index.js
@@ -1,51 +1,122 @@
 const tracking = (id) => {
   axios
     .get(`api/1.0/subscription/tracking?id=${id}`)
-    .then((res) => console.log(res.data))
+    .then((res) => console.log('Result from tracking notification', res.data))
     .catch((err) => console.log(err));
 };
 
-class SubscriptionGenerator {
-  constructor(serviceWorkerPath) {
+const channel = new BroadcastChannel('Notify-sw');
+channel.addEventListener('message', (event) => {
+  console.log('[Webpush] Receive data:', event.data);
+  if (event.data.title === 'auth') {
+    axios
+      .get(`api/1.0/subscription/verify?code=${event.data.code}`)
+      .then((res) => console.log('[Webpush] Receive verification:', res.data))
+      .catch((err) => console.log(err));
+    return;
+  }
+  tracking(event.data.notification_id);
+});
+
+class Notifier {
+  constructor(channelId, serviceWorkerPath) {
+    this.channelId = channelId;
     this.serviceWorkerPath = serviceWorkerPath;
+    this.serviceWorkerstatus = 0;
   }
   async init() {
+    // Initialize websocket client
+    if (this.socket) {
+      this.socket.connect();
+      console.log('[Websocket] Websocket client connect');
+    } else {
+      const socket = io({ transports: ['websocket', 'polling'] });
+      socket.on('connection', (data) => {
+        console.log('[Websocket] Receive:', data);
+      });
+      socket.on('push', (data) => {
+        console.log('[Websocket] Receive:', data);
+        tracking(data.notification_id);
+      });
+      this.socket = socket;
+      console.log('[Websocket] Initialize websocket client');
+    }
+
+    // Initialize && Register service worker
+    const permissionStatus = await Notification.requestPermission();
+    if (permissionStatus !== 'granted') {
+      return Promise.reject('[Webpush] Not allowed notification permission');
+    }
     await navigator.serviceWorker.register(this.serviceWorkerPath);
     this.sw = await navigator.serviceWorker.ready;
+    this.serviceWorkerstatus = 1;
+    console.log('[Webpush] Successfully register service worker');
+    return Promise.resolve();
   }
-  async askPermission() {
-    return new Promise(function (resolve, reject) {
-      const permissionResult = Notification.requestPermission(function (result) {
-        resolve(result);
-      });
 
-      if (permissionResult) {
-        permissionResult.then(resolve, reject);
-      }
-    }).then(function (permissionResult) {
-      if (permissionResult !== 'granted') {
-        throw new Error("We weren't granted permission.");
-      }
+  async unregister() {
+    // Close websocket connection
+    if (this.socket) {
+      this.socket.disconnect();
+      console.log('[Websocket] Successfully unregister socket connection');
+    }
+
+    // Unregister service worker
+    if (!this.serviceWorkerstatus) {
+      return console.log('[Webpush] No service worker to unregister');
+    }
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    registrations.forEach((registration) => {
+      registration.unregister();
     });
+    this.serviceWorkerstatus = 0;
+    return console.log('[Webpush] Successfully unregister service worker');
   }
   async getSubscription() {
+    if (!this.serviceWorkerstatus) {
+      return console.log('[Webpush] No service worker!');
+    }
     const subscription = await this.sw.pushManager.getSubscription();
     return subscription || '';
   }
   async subscribe(publicKey, userVisibleOnly) {
+    const channel_id = this.channelId;
+    // Subscribe to websocket server
+    this.socket.emit('subscribe', { channel_id });
+    console.log('[Websocket] Successfully subscribe to websocket server');
+
+    // Subscribe to webpush server
+    if (!this.serviceWorkerstatus) {
+      return console.log('[Webpush] No service worker!');
+    }
+
     const subscriptionOptions = {};
     if (!publicKey) {
-      throw new Error('Invalid publicKey');
+      return console.log('Invalid publicKey');
     }
     subscriptionOptions.applicationServerKey = new urlBase64ToUint8Array(publicKey);
     subscriptionOptions.userVisibleOnly = userVisibleOnly || false;
+    const subscription = await this.sw.pushManager.subscribe(subscriptionOptions);
+    console.log('[Webpush] Successfully subscribe to webpush server');
 
-    return this.sw.pushManager.subscribe(subscriptionOptions);
+    try {
+      const res = await axios.post('/api/1.0/subscription', { channel_id, subscription }, { headers: { 'content-type': 'application/json' } });
+      return console.log('[Webpush] Send subscription to server, ' + res.data);
+    } catch (err) {
+      return console.log(err);
+    }
   }
   async unsubscribe() {
+    // Unsubscribe to websocket server
+    this.socket.emit('unsubscribe', 'Socket unsubscribe');
+
+    // Unsubscribe to websocket server
+    if (!this.sw) {
+      return console.log('[Webpush] no service worker');
+    }
     const subscription = await this.getSubscription();
     if (!subscription) {
-      return;
+      return console.log('[Webpush] no subscription');
     }
     await subscription.unsubscribe();
     try {
@@ -54,69 +125,45 @@ class SubscriptionGenerator {
         { data: { endpoint: subscription.endpoint } },
         { headers: { 'content-type': 'application/json' } }
       );
-      return console.log('Successfully unsubscribe to server, id:' + res.data.status);
+      return console.log('[Webpush] Successfully unsubscribe to server,' + res.data);
     } catch (err) {
       return console.log(err);
     }
   }
 }
-
-const socket = io({ transports: ['websocket', 'polling'] });
-socket.on('connection', (data) => {
-  console.log('Connect socket io server', data);
-});
-socket.on('push', (data) => {
-  console.log('Receive notification from socket:', data);
-  tracking(data.notification_id);
+let myNotifier;
+$('#initial').click(async (event) => {
+  event.preventDefault();
+  const channelId = $('#channel-id').val();
+  myNotifier = new Notifier(channelId, 'notify-sw.js');
+  await myNotifier.init();
 });
 
-const newSubscription = new SubscriptionGenerator('notify-sw.js');
+$('#unregister').click(async (event) => {
+  event.preventDefault();
+  await myNotifier.unregister();
+});
 
 $('#subscribe').click(async function (event) {
   event.preventDefault();
-  const channelID = $('#channel-id').val();
   const publicKey = $('#public-key').val();
-  socket.emit('subscribe', { channel_id: channelID });
-
-  if (!'serviceWorker' in navigator) {
-    throw new Error('Service Worker Is Not Supportted');
-  }
-
-  await newSubscription.askPermission();
-  await newSubscription.init();
-  console.log(newSubscription);
-
-  const channel = new BroadcastChannel('Notify-sw');
-  channel.addEventListener('message', (event) => {
-    console.log('Received', event.data);
-    if (event.data.title === 'auth') {
-      axios
-        .get(`api/1.0/subscription/verify?code=${event.data.code}`)
-        .then((res) => console.log(res.data))
-        .catch((err) => console.log(err));
-      return;
-    }
-    tracking(event.data.notification_id);
-  });
-  let subscription = await newSubscription.getSubscription();
+  let subscription = await myNotifier.getSubscription();
   if (subscription) {
     return console.log('Subscription already exist\n' + JSON.stringify(subscription));
   }
-  subscription = await newSubscription.subscribe(publicKey, true);
-  console.log(publicKey);
-  console.log(channelID);
-  try {
-    const res = await axios.post('/api/1.0/subscription', { channel_id: channelID, subscription }, { headers: { 'content-type': 'application/json' } });
-    return console.log('Successfully subscribe to server' + res.data);
-  } catch (err) {
-    return console.log(err);
-  }
+  await myNotifier.subscribe(publicKey, true);
+  // console.log(publicKey);
+  // try {
+  //   const res = await axios.post('/api/1.0/subscription', { channel_id: channelID, subscription }, { headers: { 'content-type': 'application/json' } });
+  //   return console.log('Successfully subscribe to server, ' + res.data);
+  // } catch (err) {
+  //   return console.log(err);
+  // }
 });
 
 $('#unsubscribe').click(async function (event) {
   event.preventDefault();
-  await newSubscription.unsubscribe();
-  console.log('Unsubscribe!');
+  await myNotifier.unsubscribe();
 });
 
 $('#realtime').click(async function (event) {
@@ -162,6 +209,20 @@ $('#scheduled').click(async function (event) {
   }
 });
 
+$('#add-tag').click(async function (event) {
+  event.preventDefault();
+  const user_tag = $('#user-tag').val();
+  try {
+    const res = await axios.post(
+      '/api/1.0/notifications/scheduled',
+      { channel_id: channelID, title, body, sendType, config: { ttl, icon }, sendTime },
+      { headers: { 'content-type': 'application/json' } }
+    );
+    console.log('Push success', res);
+  } catch (err) {
+    console.log(err);
+  }
+});
 function urlBase64ToUint8Array(base64String) {
   var padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   var base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
--- public/notify-sw.js
+++ public/notify-sw.js
@@ -3,15 +3,11 @@ const channel = new BroadcastChannel('Notify-sw');
 
 // Register event listener for the 'push' event.
 self.addEventListener('push', async function (event) {
-  // Retrieve the textual payload from event.data (a PushMessageData object).
-  // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
-  // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
   const data = event.data ? event.data.json() : 'no payload';
   if (data.title === 'auth') {
     return channel.postMessage(data);
   }
   console.log(data);
-  console.log(data.config.icon);
   channel.postMessage(data);
   // Keep the service worker alive until the notification is created.
   event.waitUntil(
